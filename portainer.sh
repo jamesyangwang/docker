694a7ee0711419629f4f0167f330d9e386827b92
jw88 <jw88@live.com>
2018-06-23 23:47:39 -0700

    updated

---
+++ portainer.sh
@@ -0,0 +1,7 @@
+docker volume create portainer_data
+#linux
+#docker run -d -p 9000:9000 --name portainer --restart always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
+#windows git bash
+#docker run -d -p 9000:9000 --name portainer --restart always -v /var/run/docker.sock:/var/run/docker.sock -v C:\ProgramData\Portainer:C:\data portainer/portainer
+#docker run -d -p 9000:9000 --name portainer --restart always -v portainer_data:/data portainer/portainer -H tcp://localhost:2375
+docker run -d -p 9000:9000 --name portainer --restart always -v portainer_data:/data portainer/portainer
---
+++ readme.txt
@@ -0,0 +1,4 @@
+docker run --rm -v c:/tmp:/tmp alpine ls tmp
+
+docker run -v /c/Users/<path>:/<container path> ...
+
